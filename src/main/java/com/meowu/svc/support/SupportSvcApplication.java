b74cbd9efab4837260587a63884ca6700bd027d1
Lihaihua <583268519@qq.com>
2025-07-25 18:12:49 +0800

    <Coding> coding

---
+++ src/main/java/com/meowu/svc/support/SupportSvcApplication.java
@@ -0,0 +1,12 @@
+package com.meowu.svc.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportSvcApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(SupportSvcApplication.class, args);
+    }
+}
